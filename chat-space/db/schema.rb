d3ed504fd56605dd79b539431a6ea6f39d2663c8
yuki komatsu <yuki.k.lion@gmail.com>
2018-07-08 15:45:42 +0900

    make groups migrate file

--- chat-space/db/schema.rb
+++ chat-space/db/schema.rb
@@ -10,6 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20180708062528) do
+
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name",       null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_users_on_name", using: :btree
+  end
 
 end
