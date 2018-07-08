a9b6bbbf32e8f0242f1de8763b7c6e216648cfd3
yuki komatsu <yuki.k.lion@gmail.com>
2018-07-08 16:00:11 +0900

    create message table

--- chat-space/db/schema.rb
+++ chat-space/db/schema.rb
@@ -10,12 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180708064436) do
+ActiveRecord::Schema.define(version: 20180708064806) do
 
   create_table "groups", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",       null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "messages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "body"
+    t.string   "image"
+    t.integer  "user_id_id",  null: false
+    t.integer  "group_id_id", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["group_id_id"], name: "index_messages_on_group_id_id", using: :btree
+    t.index ["user_id_id"], name: "index_messages_on_user_id_id", using: :btree
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
