c8160cc02713a8febdf830811c02bb49c4554b87
yuki komatsu <yuki.k.lion@gmail.com>
2018-07-08 15:27:21 +0900

    make users table migtrate file

--- chat-space/db/migrate/20180708062528_create_users.rb
+++ chat-space/db/migrate/20180708062528_create_users.rb
@@ -1,7 +1,7 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
-
+      t.string :name,index:true, null:false, unique:true
       t.timestamps
     end
   end
