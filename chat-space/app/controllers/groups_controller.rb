4327f3afbb4a4723b4a26b6e617e2ef06aa507fc
yuki komatsu <yuki.k.lion@gmail.com>
2018-07-21 16:59:03 +0900

    add comment

--- chat-space/app/controllers/groups_controller.rb
+++ chat-space/app/controllers/groups_controller.rb
@@ -1,6 +1,8 @@
 class GroupsController < ApplicationController
 
   def new
+    @group = Group.new
+    @group.users << current_user
   end
 
   def create
@@ -18,4 +20,10 @@ class GroupsController < ApplicationController
 
  def update
  end
+
+ private
+  def group_params
+    params.require(:group).permit(:name, { :user_ids => [] })
+  end
+
 end
