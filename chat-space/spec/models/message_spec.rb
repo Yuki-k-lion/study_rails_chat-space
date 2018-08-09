587587d89ab27be5c56b81741d8b18084f32ef20
yuki komatsu <yuki.k.lion@gmail.com>
2018-08-09 14:49:23 +0900

    it comes error message bundler: failed to load command: rspec (/Users/Komatsu/.rbenv/versions/2.3.1/bin/rspec) NameError: uninitialized constant FactoryGirl

--- chat-space/spec/models/message_spec.rb
+++ chat-space/spec/models/message_spec.rb
@@ -2,9 +2,13 @@ require 'rails_helper'
 
 RSpec.describe Message, type: :model do
   describe '#create' do
+
+    #特定の条件でテストをグループわけしたい時にcontextを使う。
     context 'can save' do
+      #保存できる場合の記述
       it 'is valid with content' do
         expect(build(:message, image: nil)).to be_valid
+        #上書きできる。
       end
 
       it 'is valid with image' do
@@ -16,11 +20,16 @@ RSpec.describe Message, type: :model do
       end
     end
 
+
     context 'can not save' do
+      #保存できない場合の記述
       it 'is invalid without content and image' do
         message = build(:message, content: nil, image: nil)
+        #インスタンスを生成。
         message.valid?
+        #valid?メソッドを使用。
         expect(message.errors[:content]).to include('を入力してください')
+        #errorsメソッドを使用。エラーメッセージを返す。
       end
 
       it 'is invalid without group_id' do
@@ -34,6 +43,8 @@ RSpec.describe Message, type: :model do
         message.valid?
         expect(message.errors[:user]).to include('を入力してください')
       end
+
     end
+
   end
 end
