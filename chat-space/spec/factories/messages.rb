ef7bc1242f3da29fc1df5c2734c7329eac157d32
yuki komatsu <yuki.k.lion@gmail.com>
2018-08-09 15:44:26 +0900

    bugfix when run the test code, comes the error message image:nil

--- chat-space/spec/factories/messages.rb
+++ chat-space/spec/factories/messages.rb
@@ -1,7 +1,9 @@
 FactoryGirl.define do
   factory :message do
     content Faker::Lorem.sentence
-    image File.open("#{Rails.root}/public/images/no_image.jpg")
+    image {File.open("#{Rails.root}/public/images/no_image.png")}
+    #image Rack::Test::UploadedFile.new(File.join(Rails.root, '/public/images/no_image.jpg'))
+    # image 'test.png'
     user
     group
   end
