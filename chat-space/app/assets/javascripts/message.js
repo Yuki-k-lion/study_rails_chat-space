9caf02facce3e4fd7daca7da180a6df4b65ff295
yuki komatsu <yuki.k.lion@gmail.com>
2018-08-14 22:02:56 +0900

    feat:done Method render ref http://railsdoc.com/references/render

--- chat-space/app/assets/javascripts/message.js
+++ chat-space/app/assets/javascripts/message.js
@@ -1,14 +1,28 @@
-$(function(){
-  $('#new_message').on('submit', function(e){
-    e.preventDefault();
-    var formData = new FormData(this);
-    $.ajax({
-     url: url,
-     type: "POST",
-     data: formData,
-     dataType: 'json',
-     processData: false,
-     contentType: false
-    })
-  })
-})
+$(function() {
+      function buildHTML(message) {
+        var html = `<%= render :partial => "message" %>`
+        return html;
+      }
+
+      $('#new_message').on('submit', function(e) {
+        e.preventDefault();
+        var formData = new FormData(this);
+        $.ajax({
+          url: url,
+          type: "POST",
+          data: formData,
+          dataType: 'json',
+          processData: false,
+          contentType: false
+        });
+
+        .done(function(data) {
+          var html = buildHTML(data);
+          $('.messages').append(html)
+          $('.textbox').val('')
+        });
+        .fail(function(){
+          alert('error');
+        });
+
+      })
