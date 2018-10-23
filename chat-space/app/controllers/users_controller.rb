f22b21a7240de9212e242f27e22fb9bab2151cf6
yuki komatsu <yuki.k.lion@gmail.com>
2018-10-23 16:53:58 +0900

    add: アクションの設定

--- chat-space/app/controllers/users_controller.rb
+++ chat-space/app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
+  def index
+    format.json
+  end
+
   def edit
   end
+
   def update
     if current_user.update(user_params)
       redirect_to root_path
