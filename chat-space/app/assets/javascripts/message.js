4e212cfa5e384185af3f65636c5fb4140893ba0a
yuki komatsu <yuki.k.lion@gmail.com>
2018-10-23 19:54:26 +0900

    refactor: 不要な記述を削除

--- chat-space/app/assets/javascripts/message.js
+++ chat-space/app/assets/javascripts/message.js
@@ -1,14 +1,5 @@
 $(function() {
       function buildHTML(message) {
-        // var html = `<%= render :partial => 'message' %>`;
-        // var html = "<h1>test</h1>"
-        console.log(message);
-        console.log(message.message.user);
-        console.log(message.message.user.name);
-        console.log(message.message.created_at);
-        console.log(message.message.text);
-        console.log(message.message.image);
-        console.log(message.message);
         var user_name = message.message.user.name;
         var post_time = message.message.created_at;
         var content_text = ``;
@@ -17,14 +8,11 @@ $(function() {
           content_text = `
           <p class="textArea__message">
             ${message.message.text}
-          </p>
-          `;
-        }
-
+          </p>`;
+        };
         if(message.message.image){
           content_image = `<img href="message.image.url" class="textArea__image">`;
-        }
-
+        };
         var html = `
           <div class="textArea_box">
             <div class="textArea__user">
@@ -37,16 +25,9 @@ $(function() {
               ${content_text}
               ${content_image}
             </div>
-          </div>`
-        ;
+          </div>`;
         return html;
       };
-
-      // $(function(){
-      // setInterval(update, 10000);
-      // 10000ミリ秒ごとにupdateという関数を実行する
-      // });
-
       $('#new_message').on('submit', function(e) {
         e.preventDefault();
         var formData = new FormData(this);
@@ -59,25 +40,9 @@ $(function() {
           contentType: false
         })
         .done(function(data){
-          console.log($('.textArea')[0].scrollHeight);
-          console.log('done1');
-          // console.log(data.message);
-          // console.log(@message);
           var html = buildHTML(data);
-          // var html = `<%= escape_javascript render :partial => "message", locals:{message: ${data.message}} %>`;
-          //部分テンプレートを使用して簡略化しようと思ったが、なぜかうまくいかない…
-          console.log(html);
-          // $('.messages').append(html)これは間違い。
           $('.textArea').append(html);
-          // $('.textArea').append('<h2>test</h2>');部分テンプレートが読み込まれない…文字列として表示されてしまうが、これは文字列ではない。
-          // $('.textArea').html(<%= j (render @messages) %>); 記法のエラーなのでこれ以降実行されないが、表示はうまくいく。
-          // $('.textbox').val('')
           $('.sendMessage__input-text').val('');
-          // $('sendMessage__input-text').reset();
-          //reset reset is not a functionとなる。名前で競合
-          // $form = $($.rails.formSubmitSelector)
-          // $.rails.enableFormElements($form)
-          // $form.reset();
           $('.sendMessage__input-btn').prop('disabled', false);
           $('.textArea').animate({scrollTop: $('.textArea')[0].scrollHeight}, 'fast');
           console.log('done');
--- chat-space/app/views/messages/create.json.jbuilder
+++ chat-space/app/views/messages/create.json.jbuilder
@@ -2,8 +2,6 @@ json.set! :message do
   json.id @message.id
   json.user_id @message.user_id
   json.text @message.content
-  # json.user.name @message.user.name
   json.created_at @message.created_at
-  # use_name = User.find(@message.user_id)
   json.user(@message.user, :name)
 end
