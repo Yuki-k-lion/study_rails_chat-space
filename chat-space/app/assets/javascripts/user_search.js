f471dc4331d47e431028dd53185b58b0c81477da
yuki komatsu <yuki.k.lion@gmail.com>
2018-10-23 19:48:12 +0900

    refactor:不要な㏒を削除

--- chat-space/app/assets/javascripts/user_search.js
+++ chat-space/app/assets/javascripts/user_search.js
@@ -1,36 +1,24 @@
 $(function () {
   var search_user_list = $("#user-search-result");
-  console.log('search_use_list');
-  console.log(search_user_list);
-
   function builtHTMLUser(user_list) {
-    console.log(user_list);
-    console.log(user_list[0]);
-    console.log(user_list[0].name);
-    console.log(user_list[0].email);
     user_list.forEach(function(user) {
       var html = `<div class="chat-group-user clearfix">
                     <p class="chat-group-user__name">${ user.name }</p>
                     <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${ user.id }" data-user-name="${ user.name }">追加</a>
                   </div>`;
-      console.log(html);
       search_user_list.append(html);
     });
   };
-
   function builtHTML_noUser() {
     var html = `<div class="chat-group-user clearfix">
                   <p class="chat-group-user__name"> NO USER IS HERE</p>
                 </div>`;
-    console.log(html);
     search_user_list.append(html);
   }
 
   $(document).on('keyup', '#user-search-field', function(e){
     e.preventDefault();
     var input = $.trim($(this).val());
-    console.log(input);
-
     $.ajax({
      url: '/users',
      type: 'GET',
@@ -40,8 +28,6 @@ $(function () {
      dataType: 'json'
    })
    .done(function(data){
-     console.log('done');
-     console.log(data);
     $('.chat-group-user').remove();
     if(data.length !== 0){
       builtHTMLUser(data);
