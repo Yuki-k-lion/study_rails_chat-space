f2c1552b8d527c6363fe5ff7eea27b5335fc3f07
yuki komatsu <yuki.k.lion@gmail.com>
2018-10-23 18:30:57 +0900

    add: 検索結果が正常に表示される。
    実際に追加ができるのはまだ実装していない。

--- chat-space/app/assets/javascripts/user_search.js
+++ chat-space/app/assets/javascripts/user_search.js
@@ -6,18 +6,28 @@ $(document).on('turbolinks:load', function(){ //リロードしなくてもjsが
   console.log('search_use_list');
   console.log(search_user_list);
 
-  function builtHTMLUser(user) {
-    console.log(user);
-    console.log(user[0]);
-    console.log(user[0].name);
-    console.log(user[0].email);
+  function builtHTMLUser(user_list) {
+    console.log(user_list);
+    console.log(user_list[0]);
+    console.log(user_list[0].name);
+    console.log(user_list[0].email);
+    user_list.forEach(function(user) {
+      var html = `<div class="chat-group-user clearfix">
+                    <p class="chat-group-user__name">${ user.name }</p>
+                    <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${ user.id }" data-user-name="${ user.name }">追加</a>
+                  </div>`;
+      console.log(html);
+      search_user_list.append(html);
+    });
+  };
+
+  function builtHTML_noUser() {
     var html = `<div class="chat-group-user clearfix">
-                  <p class="chat-group-user__name">${ user.name }</p>
-                  <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${ user.id }" data-user-name="${ user.name }">追加</a>
-                </div>`
+                  <p class="chat-group-user__name"> NO USER IS HERE</p>
+                </div>`;
     console.log(html);
     search_user_list.append(html);
-  };
+  }
 
 
   $(document).on('keyup', '#user-search-field', function(e){ //このアプリケーション(document)の、user-search-fieldというid('#user-search-field')で、キーボードが押され指が離れた瞬間(.on('keyup'...))、eという引数を取って以下のことをしなさい(function(e))
@@ -28,7 +38,7 @@ $(document).on('turbolinks:load', function(){ //リロードしなくてもjsが
     $.ajax({ //ajax通信で以下のことを行います
      url: '/users', //urlを指定
      type: 'GET', //メソッドを指定
-     data: ('keyword=' + input), //コントローラーに渡すデータを'keyword=input(入力された文字のことですね)'にするように指定
+     data: ('keyword=' + input), //コントローラーに渡すデータを'keyword=input(入力された文字)'にするように指定
      processData: false,
      contentType: false,
      dataType: 'json' //データ形式を指定
@@ -36,10 +46,15 @@ $(document).on('turbolinks:load', function(){ //リロードしなくてもjsが
    .done(function(data){ //データを受け取ることに成功したら、dataを引数に取って以下のことする(dataには@usersが入っている状態ですね)
      console.log('done');
      console.log(data);
+
     $('.chat-group-user').remove();
+
     if(data.length !== 0){
-      builtHTMLUser(data)
-      console.log('built done');
+      builtHTMLUser(data);
+      console.log('user list built done');
+    }else{
+      builtHTML_noUser();
+      console.log('nouser built done');
     };
 
    })
