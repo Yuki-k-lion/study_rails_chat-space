0e36897a3dcd17005f3ad152e0d386d580e055e6
yuki komatsu <yuki.k.lion@gmail.com>
2018-10-28 17:21:02 +0900

    refactor:自動更新に関するファイルを切り分け

---
+++ chat-space/app/assets/javascripts/autorefresh.js
@@ -0,0 +1,61 @@
+$(function() {
+  function buildHTMLupdate(message) {
+    var user_name = message.user.name;
+    var post_time = message.created_at;
+    var content_text = ``;
+    var content_image = ``;
+    var message_id = message.id;
+    if(message.text){
+      content_text = `
+      <p class="textArea__message">
+        ${message.text}
+      </p>`;
+    };
+    if(message.image){
+      content_image = `<img href="message.image.url" class="textArea__image">`;
+    };
+    var html = `
+      <div class="textArea_box">
+        <div class="textArea__user">
+          <h3 class="textArea__name">
+            ${user_name}
+            <span class="textArea__timestamp" data-id="${message_id}">
+              ${post_time}
+            </span>
+          </h3>
+          ${content_text}
+          ${content_image}
+        </div>
+      </div>`;
+    return html;
+  };
+   function update(){
+    if($('.textArea__message')[0]){
+     var message_id = $('.textArea__message:last').data('id');
+   }else {
+     var message_id = 0;
+   };
+     $.ajax({
+       url: location.href,
+       type: "GET",
+       data: {message: { id: message_id }},
+       dataType: 'json'
+     })
+     .done(function(data){
+       $.each(data, function(i, data){
+         buildHTMLupdate(data);
+       });
+     })
+    .fail(function(XMLHttpRequest,textStatus,errorThrown){
+      console.log(XMLHttpRequest.status);
+      console.log(textStatus);
+      console.log(errorThrown);
+    })
+     .always(function(data){
+        console.log('interval always log');
+      });
+   };
+  $(function(){
+     setInterval(update, 5000);
+   });
+};
--- chat-space/app/assets/javascripts/message.js
+++ chat-space/app/assets/javascripts/message.js
@@ -29,95 +29,6 @@ $(function() {
           </div>`;
         return html;
       };
-      function buildHTMLupdate(message) {
-        var user_name = message.user.name;
-        var post_time = message.created_at;
-        var content_text = ``;
-        var content_image = ``;
-        var message_id = message.id;
-        if(message.text){
-          content_text = `
-          <p class="textArea__message">
-            ${message.text}
-          </p>`;
-        };
-        if(message.image){
-          content_image = `<img href="message.image.url" class="textArea__image">`;
-        };
-        var html = `
-          <div class="textArea_box">
-            <div class="textArea__user">
-              <h3 class="textArea__name">
-                ${user_name}
-                <span class="textArea__timestamp" data-id="${message_id}">
-                  ${post_time}
-                </span>
-              </h3>
-              ${content_text}
-              ${content_image}
-            </div>
-          </div>`;
-        return html;
-      };
-
-       function update(){
-        if($('.textArea__message')[0]){
-         var message_id = $('.textArea__message:last').data('id');
-         //一番最後にある'.textArea__message'というクラスの'id'というデータ属性を取得し、'message_id'という変数に代入
-       }else {
-         var message_id = 0;
-       };
-         $.ajax({
-           url: location.href,
-           type: "GET",
-           data: {message: { id: message_id }},
-           dataType: 'json'
-         })
-         .done(function(data){
-           $.each(data, function(i, data){
-             buildHTMLupdate(data);
-           });
-         })
-        .fail(function(XMLHttpRequest,textStatus,errorThrown){
-          // alert('error');
-          console.log(XMLHttpRequest.status);
-          console.log(textStatus);
-          console.log(errorThrown);
-        })
-         .always(function(data){
-            console.log('interval always log');
-          });
-       };
- // setInterval(function() {
- //        // var formData = new FormData(this);
- //        $.ajax({
- //          url: $(this).attr('action'),
- //          type: "POST",
- //          data: formData,
- //          dataType: 'json',
- //          processData: false,
- //          contentType: false
- //        })
- //        .done(function(data){
- //          var html = buildHTML(data);
- //          $('.textArea').append(html);
- //          // $('.sendMessage__input-text').val('');
- //          // $('.sendMessage__input-btn').prop('disabled', false);
- //          $('.textArea').animate({scrollTop: $('.textArea')[0].scrollHeight}, 'fast');
- //          console.log('done');
- //        })
- //        .fail(function(XMLHttpRequest,textStatus,errorThrown){
- //          alert('error');
- //          console.log(XMLHttpRequest.status);
- //          console.log(textStatus);
- //          console.log(errorThrown);
- //        })
- //        .always(function(){
- //          console.log('interval always log');
- //        });
- //
- //      },5000);
-
       $('#new_message').on('submit', function(e) {
         e.preventDefault();
         var formData = new FormData(this);
@@ -146,11 +57,5 @@ $(function() {
         .always(function(){
           console.log('always log');
         });
-
       });
-
-      //5000msごとにメッセージを最新のメッセージを自動的に取得更新
-      $(function(){
-         setInterval(update, 5000);
-       });
 });
